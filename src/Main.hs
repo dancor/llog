eec6fe1108100e46f5a3e6198d9cebec371af030
Daniel Corson <danl@alum.mit.edu>
2009-09-14 06:03:30 -0700

    llog working

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,16 @@
 module Main where
 
+import Data.Time
+import System.Directory
+import System.Environment
+import System.FilePath
+
 main :: IO ()
-main = do
-  putStrLn "hi"
+main = getArgs >>= \ args -> case args of
+  [log, msg] -> do
+    t <- getCurrentTime
+    h <- getHomeDirectory
+    let d = h </> "log"
+    createDirectoryIfMissing True d
+    appendFile (d </> log) $ show t ++ "\t" ++ show msg ++ "\n"
+  _ -> error "usage"
